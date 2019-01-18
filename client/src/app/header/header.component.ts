1a6b85307fc1e79b7e028956db03af1308376f76
PERSEY <dryzhov.mikhail@gmail.com>
2019-01-19 00:15:48 +0100

    add login functionality at the header

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isShowLogin = false;
 
-  constructor() { }
+  constructor(private _authservice: AuthService) { }
 
   ngOnInit() {
   }
-
+  
+  public login(username: string, password: string) {
+    this._authservice.login(username, password).subscribe((token: string) => console.log(token));
+  }
 }
