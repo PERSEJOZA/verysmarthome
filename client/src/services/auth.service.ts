c820aae3797f9c7f60bd917c75b3fa25ec23a226
PERSEY <dryzhov.mikhail@gmail.com>
2019-03-10 12:48:19 +0100

    add user class

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from 'src/assets/models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private baseURL = 'http://localhost:3000'
+  private baseURL = 'http://localhost:3001/api'
 
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<string> {
-    return this.http.post<string>(`${this.baseURL}/${username}`, password)
+    return this.http.post<string>(`${this.baseURL}/login`, new User(username, password))
   }
 }
