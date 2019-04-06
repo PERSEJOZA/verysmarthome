dd9e6f55642065f3499ed7884971b83113d43cf4
PERSEY <dryzhov.mikhail@gmail.com>
2019-04-06 11:43:16 +0200

    remove login from header

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,20 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/services/auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  isShowLogin = false;
-
-  constructor(private _authservice: AuthService) { }
-
-  ngOnInit() {
-  }
-  
-  public login(username: string, password: string) {
-    this._authservice.login(username, password).subscribe((token: string) => console.log(token));
-  }
+export class HeaderComponent {
+  public isShowUserDetails = false;
 }
