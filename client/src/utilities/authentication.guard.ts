5277c194282c63711fa0a956cecf49f28e811336
PERSEY <dryzhov.mikhail@gmail.com>
2019-04-06 11:44:04 +0200

    add authentication guards

---
+++ client/src/utilities/authentication.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationGuard implements CanActivate {
+  constructor(private _router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if(localStorage.token) {
+      return true;
+    } else {
+      this._router.navigate(['/login'], {
+        queryParams: {
+          return: state.url
+        }
+      });
+      
+      return false;
+    }
+  }
+}
