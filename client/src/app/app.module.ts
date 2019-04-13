4f640e0b5a4a2423dd037549a24af6b859d4cb59
PERSEY <dryzhov.mikhail@gmail.com>
2019-04-14 01:22:37 +0200

    organize imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HeaderCustomerInfoComponent } from './header/headerCustomerInfoComponent/headerCustomerInfoComponent.component';
-import { FoodplannerComponent } from './foodplanner/foodplanner.component';
+import { FoodPlannerComponent } from './food-planner/food-planner.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeaderCustomerInfoComponent,
-    FoodplannerComponent
+    FoodPlannerComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
