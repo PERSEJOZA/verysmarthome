146af0f1675ffd2cbd44cea7ac2576120ab33327
Mikhail <Dryzhov.Mikhail@gmail.com>
2019-05-11 22:57:11 +0100

    fix routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FoodPlannerComponent } from './food-planner/food-planner.component';
+import { LoginComponent } from './login/login.component';
+import { AuthenticationGuard } from 'src/utilities/authentication.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'food-planner',
+    canActivate: [AuthenticationGuard],
+    component: FoodPlannerComponent
+  },
+  { path: '**', component: FoodPlannerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FoodPlannerComponent } from './food-planner/food-planner.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FoodPlannerComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
