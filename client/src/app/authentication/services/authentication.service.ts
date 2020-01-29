0fcefc051a7f5f88b01da15efbcc632c0281dd02
Mikhail <Dryzhov.Mikhail@gmail.com>
2020-01-29 23:42:17 +0100

    stylign changes

--- client/src/app/authentication/services/authentication.service.ts
+++ client/src/app/authentication/services/authentication.service.ts
@@ -7,15 +7,13 @@ import {User} from '../models/user';
 
 @Injectable()
 export class AuthenticationService {
-  private baseURL = this.configuration.nestJSServerUrl;
-
   constructor(private http: HttpClient, private configuration: ConfigurationService) {}
 
-  login(username: string, password: string): Observable<string> {
-    return this.http.post<string>(`${this.baseURL}/login`, new User(username, password));
+  public login(username: string, password: string): Observable<string> {
+    return this.http.post<string>(`${this.configuration.nestJSServerUrl}/login`, new User(username, password));
   }
 
-  register(username: string, password: string): Observable<string> {
-    return this.http.post<string>(`${this.baseURL}/register`, new User(username, password));
+  public register(username: string, password: string): Observable<string> {
+    return this.http.post<string>(`${this.configuration.nestJSServerUrl}/register`, new User(username, password));
   }
 }
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  nestJSServerUrl: 'http://localhost:3000/api'
+  nestJSServerUrl: 'http://localhost:3000/api/v1'
 };
 
 /*
